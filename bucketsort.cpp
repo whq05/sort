a3988812e75072617154b0f64f91053e2dce7ae0
whq <1469325238@qq.com>
2025-04-26 01:51:43 +0800

    桶排序

--- bucketsort.cpp
+++ bucketsort.cpp
@@ -7,11 +7,10 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>  
-#include <cstring>  // memset
+#include <algorithm>
+#include <cstring> // memset
 
 using namespace std;
-
 
 // 采用两层循环实现冒泡排序的方法
 // 参数arr是待排序数组的首地址，len是数组元素的个数
@@ -23,21 +22,22 @@ void bubblesort(int *arr, int len)
         bool swapped = false;
         for (j = 0; j < i; j++)
         {
-            if (arr[j] > arr[j + 1]) 
+            if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
         }
-        if (!swapped) return;
+        if (!swapped)
+            return;
     }
 }
 
 // 桶排序主函数，参数arr是待排序数组的首地址，len是数组元素的个数
 void bucketsort(int *arr, int len)
 {
-    int bucket[5][5];   // 分配五个桶
-    int bucketsize[5];  // 每个桶中元素个数的计数器
+    int bucket[5][5];  // 分配五个桶
+    int bucketsize[5]; // 每个桶中元素个数的计数器
 
     // 初始化桶和桶计数器
     memset(bucket, 0, sizeof(bucket));
@@ -67,10 +67,9 @@ void bucketsort(int *arr, int len)
     }
 }
 
-
 int main()
 {
-    vector<int> vec{21,3,30,44,15,36,6,10,9,19,25,48,5,23,47};
+    vector<int> vec{21, 3, 30, 44, 15, 36, 6, 10, 9, 19, 25, 48, 5, 23, 47};
     int len = vec.size();
     bucketsort(vec.data(), len);
     cout << "排序后的数组为：";
